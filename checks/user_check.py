0cadcc64292688f505d87957b3005691894a3341
Iwlj4s <elizavetalenn@mail.ru>
2024-08-14 13:55:40 +0300

    Test commit

---
+++ checks/user_check.py
@@ -0,0 +1,19 @@
+import requests
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+def track_number_check(user_track_numbers: str, track_numbers_amount: int):
+
+    print(f"Check, user_track_numbers: {user_track_numbers}, track_numbers_amount: {track_numbers_amount}")
+
+    if len(str(user_track_numbers)) == int(track_numbers_amount):
+        print(True)
+        return True
+    
+    else:
+        print(False)
+        return False
+    
